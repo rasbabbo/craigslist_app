5291e0b616573188398911fd0eb8a813c0a20763
rasbabbo <rasbabbo@yahoo.com>
2014-08-12 22:06:11 -0700

    some headway

--- scrapper.rb
+++ scrapper.rb
@@ -1,36 +1,72 @@
 # scrapper.rb
 require 'nokogiri'
 require 'open-uri'
+require 'awesome_print'
 
+# pushresults into an array
+# pass array into date search
+# pass filtered dates through the row search
+#   map over to keep the original array
+
+
+
+
+def get_page_results
+  html = open("today.html")
+  doc = Nokogiri::HTML(open(html))
+end
 
 
 def filter_links(rows, regex)
+  dogRows = rows.select do |row|
+    row.css(".hrdlnk").text.match("/pup|puppy|puppies|dog/")
+
+
   # takes in rows and returns uses
   # regex to only return links 
   # that have "pup", "puppy", or "dog"
   # keywords
 end
 
+
+
 def get_todays_rows(doc, date_str)
-  #  1.) open chrome console to look in inside p.row to see
-  #  if there is some internal date related content
 
-  #  2.) figure out the class that you'll need to select the
-  #   date from a row
+  rows = doc.css(".row")
+
+  todaysRows = rows.select do |row|
+  row.css("span.date").text == date_str
+end
+
+  results.push(rows)
+  puts results
 
 end
 
-def get_page_results
-  url = "http://sfbay.craigslist.org/sfc/pet/"
-  Nokogiri::HTML(open(url))
-end
+
+#   #  1.) open chrome console to look in inside p.row to see
+#   #  if there is some internal date related content
+
+#   #  2.) figure out the class that you'll need to select the
+#   #   date from a row
+
+
+
 
 def search(date_str)
-  get_page_results
+  # go through the results returning only those with the dates;
+  # get_page_result
 end
+
+
+
+
+
+
+
 
 # want to learn more about 
 # Time in ruby??
 #   http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/Date.html#strftime-method
 today = Time.now.strftime("%b %d")
-search(today)
+results = search(today)
