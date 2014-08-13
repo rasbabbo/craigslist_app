fd1299fe05f4ed86cc8b274deb5ce176d7c93944
rasbabbo <rasbabbo@yahoo.com>
2014-08-13 09:22:02 -0700

    not working

--- scrapper.rb
+++ scrapper.rb
@@ -11,51 +11,49 @@ require 'awesome_print'
 
 
 
-def get_page_results
-  html = open("today.html")
-  doc = Nokogiri::HTML(open(html))
-end
-
-
-def filter_links(rows, regex)
-  dogRows = rows.select do |row|
+def filter_links(rows)
+  dogs = rows.select do |row|
     row.css(".hrdlnk").text.match("/pup|puppy|puppies|dog/")
-
-
+  end
+end
   # takes in rows and returns uses
   # regex to only return links 
   # that have "pup", "puppy", or "dog"
   # keywords
-end
-
-
 
 def get_todays_rows(doc, date_str)
 
   rows = doc.css(".row")
 
-  todaysRows = rows.select do |row|
-  row.css("span.date").text == date_str
-end
-
-  results.push(rows)
-  puts results
-
+  currentDate = rows.select do |row|
+    row.css("span.date").text == date_str
+  end
+  filter_links(currentDate)
 end
 
 
 #   #  1.) open chrome console to look in inside p.row to see
 #   #  if there is some internal date related content
-
 #   #  2.) figure out the class that you'll need to select the
 #   #   date from a row
 
+def get_page_results(date_str)
+  url = open("today.html")
+  doc = Nokogiri::HTML(url)
+  get_todays_rows(doc, date_str)
+end
 
 
 
 def search(date_str)
   # go through the results returning only those with the dates;
   # get_page_result
+  results = get_page_results(date_str)
+  rows = results.map do |result| {
+     title: result.css(".hrdlnk").text
+    }
+  end
+  rows
 end
 
 
@@ -70,3 +68,5 @@ end
 #   http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/Date.html#strftime-method
 today = Time.now.strftime("%b %d")
 results = search(today)
+puts results
+
